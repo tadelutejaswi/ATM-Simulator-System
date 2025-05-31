8805218d2a47f5e480b8568e9ab64fab8da76de4
tadelutejaswi <tadelutejaswi709@gmail.com>
2025-05-31 09:52:34 +0530

    Fixed issue in Signup.java

--- ATM-Simulator-System/src/ASimulatorSystem/Signup.java
+++ ATM-Simulator-System/src/ASimulatorSystem/Signup.java
@@ -4,34 +4,30 @@ import java.awt.*;
 import java.awt.event.*;
 import javax.swing.*;
 import java.sql.*;
-import com.toedter.calendar.JDateChooser;
 import java.util.*;
 
-public class Signup extends JFrame implements ActionListener{
+public class Signup extends JFrame implements ActionListener {
     
-    JLabel l1,l2,l3,l4,l5,l6,l7,l8,l9,l10,l11,l12,l13,l14,l15;
-    JTextField t1,t2,t3,t4,t5,t6,t7;
-    JRadioButton r1,r2,r3,r4,r5;
+    JLabel l1, l2, l3, l4, l5, l6, l7, l8, l9, l10, l11, l12;
+    JTextField t1, t2, t3, t4, t5, t6, t7, t8, t9, t10; // Added fields for date (day, month, year)
+    JRadioButton r1, r2, r3, r4, r5;
     JButton b;
-    JDateChooser dateChooser;
-    
     
     Random ran = new Random();
     long first4 = (ran.nextLong() % 9000L) + 1000L;
     String first = "" + Math.abs(first4);
     
-    Signup(){
-        
+    Signup() {
         setTitle("NEW ACCOUNT APPLICATION FORM");
         
         ImageIcon i1 = new ImageIcon(ClassLoader.getSystemResource("ASimulatorSystem/icons/logo.jpg"));
         Image i2 = i1.getImage().getScaledInstance(100, 100, Image.SCALE_DEFAULT);
         ImageIcon i3 = new ImageIcon(i2);
-        JLabel l11 = new JLabel(i3);
-        l11.setBounds(20, 0, 100, 100);
-        add(l11);
+        JLabel logo = new JLabel(i3);
+        logo.setBounds(20, 0, 100, 100);
+        add(logo);
         
-        l1 = new JLabel("APPLICATION FORM NO. "+first);
+        l1 = new JLabel("APPLICATION FORM NO. " + first);
         l1.setFont(new Font("Raleway", Font.BOLD, 38));
         
         l2 = new JLabel("Page 1: Personal Details");
@@ -67,38 +63,36 @@ public class Signup extends JFrame implements ActionListener{
         l12 = new JLabel("State:");
         l12.setFont(new Font("Raleway", Font.BOLD, 20));
         
-        l13 = new JLabel("Date");
-        l13.setFont(new Font("Raleway", Font.BOLD, 14));
-        
-        l14 = new JLabel("Month");
-        l14.setFont(new Font("Raleway", Font.BOLD, 14));
-        
-        l15 = new JLabel("Year");
-        l15.setFont(new Font("Raleway", Font.BOLD, 14));
-        
-        
         t1 = new JTextField();
         t1.setFont(new Font("Raleway", Font.BOLD, 14));
         
         t2 = new JTextField();
         t2.setFont(new Font("Raleway", Font.BOLD, 14));
         
-        t3 = new JTextField();
+        // Date of birth fields (day, month, year)
+        t3 = new JTextField("DD");
         t3.setFont(new Font("Raleway", Font.BOLD, 14));
         
-        t4 = new JTextField();
+        t4 = new JTextField("MM");
         t4.setFont(new Font("Raleway", Font.BOLD, 14));
         
-        t5 = new JTextField();
+        t5 = new JTextField("YYYY");
         t5.setFont(new Font("Raleway", Font.BOLD, 14));
         
-        t6 = new JTextField();
+        t6 = new JTextField(); // For email
         t6.setFont(new Font("Raleway", Font.BOLD, 14));
         
-        t7 = new JTextField();
+        t7 = new JTextField(); // For address
         t7.setFont(new Font("Raleway", Font.BOLD, 14));
         
-       
+        t8 = new JTextField(); // For city
+        t8.setFont(new Font("Raleway", Font.BOLD, 14));
+        
+        t9 = new JTextField(); // For pincode
+        t9.setFont(new Font("Raleway", Font.BOLD, 14));
+        
+        t10 = new JTextField(); // For state
+        t10.setFont(new Font("Raleway", Font.BOLD, 14));
         
         b = new JButton("Next");
         b.setFont(new Font("Raleway", Font.BOLD, 14));
@@ -134,151 +128,152 @@ public class Signup extends JFrame implements ActionListener{
         groupstatus.add(r4);
         groupstatus.add(r5);
         
-        dateChooser = new JDateChooser();
-	//dateChooser.setBorder(new LineBorder(new Color(0, 0, 0), 1, true));
-	dateChooser.setForeground(new Color(105, 105, 105));
-	dateChooser.setBounds(137, 337, 200, 29);
-	add(dateChooser);
-        
         setLayout(null);
-        l1.setBounds(140,20,600,40);
+        l1.setBounds(140, 20, 600, 40);
         add(l1);
         
-        l2.setBounds(290,80,600,30);
+        l2.setBounds(290, 80, 600, 30);
         add(l2);
         
-        l3.setBounds(100,140,100,30);
+        l3.setBounds(100, 140, 100, 30);
         add(l3);
         
-        t1.setBounds(300,140,400,30);
+        t1.setBounds(300, 140, 400, 30);
         add(t1);
         
-        l4.setBounds(100,190,200,30);
+        l4.setBounds(100, 190, 200, 30);
         add(l4);
         
-        t2.setBounds(300,190,400,30);
+        t2.setBounds(300, 190, 400, 30);
         add(t2);
         
-        l5.setBounds(100,240,200,30);
+        l5.setBounds(100, 240, 200, 30);
         add(l5);
         
-        dateChooser.setBounds(300, 240, 400, 30);
-        
-        l6.setBounds(100,290,200,30);
-        add(l6);
-        
-        r1.setBounds(300,290,60,30);
-        add(r1);
-        
-        r2.setBounds(450,290,90,30);
-        add(r2);
-        
-        l7.setBounds(100,340,200,30);
-        add(l7);
-        
-        t3.setBounds(300,340,400,30);
+        // Date of birth fields
+        t3.setBounds(300, 240, 50, 30);
         add(t3);
         
-        l8.setBounds(100,390,200,30);
-        add(l8);
-        
-        r3.setBounds(300,390,100,30);
-        add(r3);
-        
-        r4.setBounds(450,390,100,30);
-        add(r4);
-        
-        r5.setBounds(635,390,100,30);
-        add(r5);
-        
-        
-        
-        l9.setBounds(100,440,200,30);
-        add(l9);
-        
-        t4.setBounds(300,440,400,30);
+        t4.setBounds(360, 240, 50, 30);
         add(t4);
         
-        l10.setBounds(100,490,200,30);
-        add(l10);
-        
-        t5.setBounds(300,490,400,30);
+        t5.setBounds(420, 240, 80, 30);
         add(t5);
         
-        l11.setBounds(100,540,200,30);
-        add(l11);
+        l6.setBounds(100, 290, 200, 30);
+        add(l6);
         
-        t6.setBounds(300,540,400,30);
+        r1.setBounds(300, 290, 60, 30);
+        add(r1);
+        
+        r2.setBounds(450, 290, 90, 30);
+        add(r2);
+        
+        l7.setBounds(100, 340, 200, 30);
+        add(l7);
+        
+        t6.setBounds(300, 340, 400, 30);
         add(t6);
         
-        l12.setBounds(100,590,200,30);
-        add(l12);
+        l8.setBounds(100, 390, 200, 30);
+        add(l8);
         
-        t7.setBounds(300,590,400,30);
+        r3.setBounds(300, 390, 100, 30);
+        add(r3);
+        
+        r4.setBounds(450, 390, 100, 30);
+        add(r4);
+        
+        r5.setBounds(635, 390, 100, 30);
+        add(r5);
+        
+        l9.setBounds(100, 440, 200, 30);
+        add(l9);
+        
+        t7.setBounds(300, 440, 400, 30);
         add(t7);
         
-        b.setBounds(620,660,80,30);
+        l10.setBounds(100, 490, 200, 30);
+        add(l10);
+        
+        t8.setBounds(300, 490, 400, 30);
+        add(t8);
+        
+        l11.setBounds(100, 540, 200, 30);
+        add(l11);
+        
+        t9.setBounds(300, 540, 400, 30);
+        add(t9);
+        
+        l12.setBounds(100, 590, 200, 30);
+        add(l12);
+        
+        t10.setBounds(300, 590, 400, 30);
+        add(t10);
+        
+        b.setBounds(620, 660, 80, 30);
         add(b);
         
-        b.addActionListener(this); 
+        b.addActionListener(this);
         
         getContentPane().setBackground(Color.WHITE);
         
-        setSize(850,800);
-        setLocation(500,120);
+        setSize(850, 800);
+        setLocation(500, 120);
         setVisible(true);
     }
     
-    public void actionPerformed(ActionEvent ae){
-        
+    public void actionPerformed(ActionEvent ae) {
         String formno = first;
         String name = t1.getText();
         String fname = t2.getText();
-        String dob = ((JTextField) dateChooser.getDateEditor().getUiComponent()).getText();
+        
+        // Combine date fields
+        String dob = t3.getText() + "/" + t4.getText() + "/" + t5.getText();
+        
         String gender = null;
-        if(r1.isSelected()){ 
+        if (r1.isSelected()) {
             gender = "Male";
-        }else if(r2.isSelected()){ 
+        } else if (r2.isSelected()) {
             gender = "Female";
         }
-            
-        String email = t3.getText();
+        
+        String email = t6.getText();
         String marital = null;
-        if(r3.isSelected()){ 
+        if (r3.isSelected()) {
             marital = "Married";
-        }else if(r4.isSelected()){ 
+        } else if (r4.isSelected()) {
             marital = "Unmarried";
-        }else if(r5.isSelected()){ 
+        } else if (r5.isSelected()) {
             marital = "Other";
         }
-           
-        String address = t4.getText();
-        String city = t5.getText();
-        String pincode = t6.getText();
-        String state = t7.getText();
         
-
-        try{
-           
-            if(t6.getText().equals("")){
+        String address = t7.getText();
+        String city = t8.getText();
+        String pincode = t9.getText();
+        String state = t10.getText();
+        
+        try {
+            if (name.equals("") || fname.equals("") || dob.equals("") || gender == null || 
+                email.equals("") || marital == null || address.equals("") || 
+                city.equals("") || pincode.equals("") || state.equals("")) {
                 JOptionPane.showMessageDialog(null, "Fill all the required fields");
-            }else{
+            } else {
                 Conn c1 = new Conn();
-                String q1 = "insert into signup values('"+formno+"','"+name+"','"+fname+"','"+dob+"','"+gender+"','"+email+"','"+marital+"','"+address+"','"+city+"','"+pincode+"','"+state+"')";
+                String q1 = "insert into signup values('" + formno + "','" + name + "','" + fname + "','" + 
+                             dob + "','" + gender + "','" + email + "','" + marital + "','" + 
+                             address + "','" + city + "','" + pincode + "','" + state + "')";
                 c1.s.executeUpdate(q1);
                 
                 new Signup2(first).setVisible(true);
                 setVisible(false);
             }
-            
-        }catch(Exception e){
-             e.printStackTrace();
+        } catch (Exception e) {
+            e.printStackTrace();
         }
-        
     }
     
-    
-    public static void main(String[] args){
+    public static void main(String[] args) {
         new Signup().setVisible(true);
     }
 }
